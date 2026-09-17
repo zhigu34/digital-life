4e3745b75764ae84478cc557918df740f22fd1fc
zhigu34 <zhi.gu@foxmail.com>
2026-09-17 16:10:10 +0800

    refactor: add shows feature domain boundary

---
+++ frontend/src/features/shows/domain.ts
@@ -0,0 +1,18 @@
+export {
+  applyShowMetadata,
+  filterShows,
+  groupShowsByType,
+  normalizeShowPayload,
+  showAdvanceDisabled,
+  showMetadataPatch,
+  showProgressPercent,
+} from "../../shows";
+
+export type {
+  NormalizeShowResult,
+  ShowFilter,
+  ShowFormDraft,
+  ShowMetadataPatch,
+  ShowMetadataResult,
+  ShowTypeGroup,
+} from "../../shows";
